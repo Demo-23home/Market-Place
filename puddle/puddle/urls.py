42934f4e044b5bd57e55d26f0cdf1850778ed8c8
zeyad slama <zeyadslama23@gamil.com>
2023-07-01 15:49:44 +0300

    adding item detail ,related items

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -20,9 +20,11 @@ from django.contrib import admin
 from django.urls import path
 from core.views import index
 from core.views import contact
+from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name="index"),
     path('contact/',contact,name="contact"),
+    path('items/',include('item.urls'))
     
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
